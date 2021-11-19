ab5b609176790fc3e1f00be6d89bbe6170475666
scteauxghne <94629823+scteauxghne@users.noreply.github.com>
2021-11-19 13:31:40 -0600

    Update Session.java

--- SelfQuiz_v2/src/selfquiz/Session.java
+++ SelfQuiz_v2/src/selfquiz/Session.java
@@ -13,13 +13,25 @@ import java.util.ArrayList;
  */
 public class Session
 {
+    private boolean generalHints;
+    private boolean dateHints;
+    private boolean actionHints;
+    private boolean associationHints;
     private int startingTerms;
     private ArrayList<Term> remainingTerms;
     private ArrayList<Term> termLog;
     private Term currentTerm;
+    private ArrayList<String> termHints;
+    private ArrayList<String> usedTermHints;
+    private int hintsUsed;
+    private float points;
     
-    Session(ArrayList<Deck> decks)
+    Session(ArrayList<Deck> decks, boolean generalHints, boolean dateHints, boolean actionHints, boolean associationHints)
     {
+        this.generalHints = generalHints;
+        this.dateHints = dateHints;
+        this.actionHints = actionHints;
+        this.associationHints = associationHints;
         remainingTerms = decks.get(0).getTerms();
         for (int i = 1; i < decks.size(); i++)
         {
@@ -30,8 +42,12 @@ public class Session
         }
         startingTerms = remainingTerms.size();
     }
-    Session(ArrayList<Deck> decks, ArrayList<Term> terms)
+    Session(ArrayList<Deck> decks, ArrayList<Term> terms, boolean generalHints, boolean dateHints, boolean actionHints, boolean associationHints))
     {
+        this.generalHints = generalHints;
+        this.dateHints = dateHints;
+        this.actionHints = actionHints;
+        this.associationHints = associationHints;
         remainingTerms = decks.get(0).getTerms();
         for (int i = 1; i < decks.size(); i++)
         {
@@ -50,14 +66,32 @@ public class Session
     public int getStartingTerms() { return startingTerms; }
     public ArrayList<Term> getRemainingTerms() { return remainingTerms; }
     public ArrayList<Term> getTermLog() { return termLog; }
+    public Term getCurrentTerm() { return currentTerm; }
+    
     public void getNewTerm()
     {
         currentTerm = remainingTerms.get((int)(Math.random() * (remainingTerms.size() -1)));
-    }
-    public Term getCurrentTerm() { return currentTerm; }
+        if (generalHints == true)
+        {
+            for (int i = o; i < currentTerm.getGeneralHints().size(); i++)
+            {
+                termHints.add(currentTerm.getGeneralHints().get(i));
+            }
+        }
+        if (dateHints == true)
+        {
+            for (int i = o; i < currentTerm.getDateHints().size(); i++)
+            {
+                termHints.add(currentTerm.getDateHints().get(i));
+            }
+        }
+ 
+    public String getHint
     public void useTerm(Term t)
     {
         remainingTerms.remove(t);
         termLog.add(t);
+        termHints = null;
+        usedTermHints = null;
     }
 }
